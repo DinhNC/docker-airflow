888ef68297dbd2549d78e5bacc7a7896ef6189e6
y2k-shubham <y2k.shubhamgupta@gmail.com>
2018-07-06 13:09:06 +0530

    debug

--- dags/xcom_1.py
+++ dags/xcom_1.py
@@ -9,25 +9,26 @@ from datetime import datetime, timedelta
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
-    "start_date": datetime(2018, 7, 5),
+    "start_date": datetime(year=2018, month=7, day=6),
     "email": ["airflow@airflow.com"],
     "email_on_failure": False,
     "email_on_retry": False,
     "retries": 1,
     "retry_delay": timedelta(seconds=30),
+    "provide_context": True
     # 'queue': 'bash_queue',
     # 'pool': 'backfill',
     # 'priority_weight': 10,
     # 'end_date': datetime(2016, 1, 1),
 }
-dag_id = 'xcom-1-v3'
+dag_id = 'xcom-1-v6'
 xcom_list_int = [1, 2, 3]
 xcom_list_str = ['one', 'two', 'three']
 xcom_key = 'xcom_key'
 
 
 # methods
-def push_by_return():
+def push_by_return(**kwargs):
     return (xcom_list_int, xcom_list_str)
 
 def push(key=None, **kwargs):
@@ -44,7 +45,7 @@ def pull(task_ids=None, dag_id=None, key=None, **kwargs):
 
 # dag & tasks
 dag = DAG(dag_id=dag_id, default_args=default_args,
-          schedule_interval='@once', catchup=False)
+          schedule_interval=None, catchup=True)
 
 py_op_push_by_return = PythonOperator(task_id='py_op_push_by_return',
                                       dag=dag, python_callable=push_by_return)
@@ -59,10 +60,10 @@ py_op_pull_by_task_ids = PythonOperator(task_id='py_op_pull_by_task_ids',
                                         op_args=[['py_op_push_by_return', 'py_op_push_with_key']])
 py_op_pull_by_dag_id = PythonOperator(task_id='py_op_pull_by_dag_id',
                                       dag=dag, python_callable=pull,
-                                      op_args=[None, dag_id])
+                                      op_args=['py_op_push_by_return', dag_id])
 py_op_pull_by_key = PythonOperator(task_id='py_op_pull_by_key',
                                    dag=dag, python_callable=pull,
-                                   op_args=[None, None, xcom_key + 'str'])
+                                   op_args=['py_op_push_with_key', None, xcom_key + 'str'])
 
 
 # relationships
