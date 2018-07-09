8f0ae449ab875e6bbcc1a167ef644a7293b73388
y2k-shubham <y2k.shubhamgupta@gmail.com>
2018-07-09 11:53:03 +0530

    xcom 1 - partial, xcom 2 - incomplete

--- dags/xcom_1.py
+++ dags/xcom_1.py
@@ -21,24 +21,22 @@ default_args = {
     # 'priority_weight': 10,
     # 'end_date': datetime(2016, 1, 1),
 }
-dag_id = 'xcom-1-v6'
+dag_id = 'xcom-1-v7'
 xcom_list_int = [1, 2, 3]
 xcom_list_str = ['one', 'two', 'three']
-xcom_key = 'xcom_key'
+xcom_key = 'xcom_key_'
 
 
 # methods
 def push_by_return(**kwargs):
     return (xcom_list_int, xcom_list_str)
 
-def push(key=None, **kwargs):
-    if key is None:
-        kwargs['ti'].xcom_push(value=(xcom_list_int, xcom_list_str))
-    else:
-        kwargs['ti'].xcom_push(key=key+'int', value=xcom_list_int)
-        kwargs['ti'].xcom_push(key=key+'str', value=xcom_list_str)
+def push_by_key(key=None, **kwargs):
+    kwargs['ti'].xcom_push(key=key+'int_str', value=(xcom_list_int, xcom_list_str))
+    kwargs['ti'].xcom_push(key=key+'str', value=xcom_list_str)
+    kwargs['ti'].xcom_push(key=key+'int', value=xcom_list_int)
 
-def pull(task_ids=None, dag_id=None, key=None, **kwargs):
+def pull(task_ids, dag_id=None, key=None, **kwargs):
     print('pulled_msg: ', end='')
     print(kwargs['ti'].xcom_pull(task_ids=task_ids, dag_id=dag_id, key=key))
 
@@ -49,33 +47,29 @@ dag = DAG(dag_id=dag_id, default_args=default_args,
 
 py_op_push_by_return = PythonOperator(task_id='py_op_push_by_return',
                                       dag=dag, python_callable=push_by_return)
-py_op_push_without_key = PythonOperator(task_id='py_op_push_without_key',
-                                        dag=dag, python_callable=push)
-py_op_push_with_key = PythonOperator(task_id='py_op_push_with_key',
-                                     dag=dag, python_callable=push,
-                                     op_args=[xcom_key])
+py_op_push_by_key = PythonOperator(task_id='py_op_push_by_key',
+                                   dag=dag, python_callable=push_by_key,
+                                   op_args=[xcom_key])
 
 py_op_pull_by_task_ids = PythonOperator(task_id='py_op_pull_by_task_ids',
                                         dag=dag, python_callable=pull,
-                                        op_args=[['py_op_push_by_return', 'py_op_push_with_key']])
+                                        op_args=['py_op_push_by_key'])
 py_op_pull_by_dag_id = PythonOperator(task_id='py_op_pull_by_dag_id',
                                       dag=dag, python_callable=pull,
-                                      op_args=['py_op_push_by_return', dag_id])
+                                      op_args=[['py_op_push_by_key'], dag_id])
 py_op_pull_by_key = PythonOperator(task_id='py_op_pull_by_key',
                                    dag=dag, python_callable=pull,
-                                   op_args=['py_op_push_with_key', None, xcom_key + 'str'])
+                                   op_args=['py_op_push_by_key', None, xcom_key+'int_str'])
 
 
 # relationships
 sleep_op_1 = BashOperator(task_id='sleep_op_1', dag=dag,
                           bash_command='sleep 5')
-sleep_op_1.set_downstream([py_op_push_by_return, py_op_push_without_key,
-                           py_op_push_with_key])
+sleep_op_1.set_downstream([py_op_push_by_return, py_op_push_by_key])
 
 sleep_op_2 = BashOperator(task_id='sleep_op_2', dag=dag,
                           bash_command='sleep 5')
-sleep_op_2.set_upstream([py_op_push_by_return, py_op_push_without_key,
-                         py_op_push_with_key])
+sleep_op_2.set_upstream([py_op_push_by_return, py_op_push_by_key])
 sleep_op_2.set_downstream([py_op_pull_by_task_ids, py_op_pull_by_dag_id,
                            py_op_pull_by_key])
 
--- dags/xcom_2.py
+++ dags/xcom_2.py
@@ -19,8 +19,17 @@ default_args = {
 }
 in_file_path = 'in_files/'
 out_file_path = 'out_files/'
+task_id_push_file_to_xcom = 'push_file_to_xcom_'
+task_id_push_sum_to_xcom = 'push_sum_to_xcom_'
 
+# xcom methods
+def push_list(key_name, list_to_push, **kwargs):
+  kwargs['ti'].xcom_push(key=key_name, value=list_to_push)
 
+def pull_list(task_ids, key_name, **kwargs):
+  return kwargs['ti'].xcom_pull(task_ids=task_ids, key=key_name)
+
+# file read-write methods
 def read_list(file_path):
     with open(file_path) as file:
         lines = file.read().splitlines()
@@ -31,9 +40,39 @@ def write_list(file_path, lines):
     with open(file_path, mode='wt') as file:
         file.write('\n'.join([str(line) for line in lines]))
 
+# utility methods
 def add_lists(list_1_name, list_2_name):
     sum_list = [(ele_1 + ele_2) for ele_1, ele_2 in list(zip(list_1, list_2))]
     return sum_list
+
+def extract_file_name(file_path):
+  file_name = file_path[file_path.rfind('/'):][1:]
+  return file_name
+
+# complete tasks
+def push_file_to_xcom(file_path, **kwargs):
+  file_name = extract_file_name(file_path)
+  list_to_push = read_list(file_path)
+  push_list(file_name, list_to_push, kwargs)
+
+def push_sum_to_xcom(file_path_1, file_path_2, **kwargs):
+  file_name_1 = extract_file_name(file_path_1)
+  file_name_2 = extract_file_name(file_path_2)
+  file_name_12 = file_name_1 + file_name_2
+
+  task_ids_1 = [task_id_push_file_to_xcom + file_name_1]
+  task_ids_2 = [task_id_push_file_to_xcom + file_name_2]
+
+  list_1 = pull_list(task_ids_1, file_name_1, kwargs)
+  list_2 = pull_list(task_ids_2, file_name_2, kwargs)
+  list_sum = add_lists(list_1, list_2)
+
+  push_list(file_name_12, list_sum, kwargs)
+
+def write_xcom_to_file(task_ids, file_path, **kwargs):
+  file_name = extract_file_name(file_path)
+  list_to_write = pull_list(task_ids, file_name, kwargs)
+  write_list(file_path, list_to_write)
 
 
 dag = DAG(dag_id="xcom-2-v1", default_args=default_args,
