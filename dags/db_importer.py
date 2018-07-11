0866b378d415461a1f07a9eb8f67698ad155900d
y2k-shubham <y2k.shubhamgupta@gmail.com>
2018-07-11 16:59:01 +0530

    db_importer [incomplete]

---
+++ dags/db_importer.py
@@ -0,0 +1,91 @@
+import airflow
+import airflow.utils.helpers
+from airflow.operators.python_operator import ShortCircuitOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.subdag_operator import SubDagOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.models import DAG
+
+from datetime import datetime, timedelta
+import time
+import random
+
+# args
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(year=2018, month=7, day=10),
+    "email": ["airflow@airflow.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(seconds=30),
+    "provide_context": False,
+    "catchup": False
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+}
+did_importer_prefix = 'importer_v1_'
+tid_prefix_check = 'check_'
+tid_prefix_spark = 'spark_submit_'
+tid_prefix_distcp = 'distcp_'
+db_names = ['zomato_ads', 'diy_db', 'zomato4', 'zbilling', 'ztransactions',
+            'chat', 'o2_internal', 'zpush']
+
+
+# helper methods
+def mimic_task(task_name, success_percent=100, sleep_duration=0):
+    time.sleep(sleep_duration)
+    if (random.randint(1, 101) <= success_percent):
+        print('%s succeeded' % (task_name))
+        return True
+    else:
+        print('%s failed' % (task_name))
+        return False
+
+
+# callable methods
+def check_sync_enabled(db_name, **kwargs):
+    return mimic_task('check_sync_enabled for %s' % db_name, 100, 1)
+
+def spark_submit(db_name, **kwargs):
+    return mimic_task('spark_submit for %s' % db_name, 70, 5)
+
+def distcp(db_name, **kwargs):
+    if mimic_task('distcp for %s' % db_name, 60, 2) == False:
+        raise Exception('Exception in dictcp for %s' % db_name)
+
+
+# db_importer dags
+def create_dag(did_prefix, db_name):
+    did_importer = did_prefix + db_name
+    default_args_copy = default_args.copy()
+    default_args_copy['trigger_rule'] = 'all_done'
+    dag = DAG(dag_id=did_importer,
+              default_args=default_args_copy,
+              schedule_interval=None)
+    op_args=[db_name]
+
+    tid_check = tid_prefix_check + db_name
+    sc_op_check = ShortCircuitOperator(task_id=tid_check, dag=dag,
+                                       python_callable=check_sync_enabled,
+                                       op_args=op_args)
+
+    tid_spark = tid_prefix_spark + db_name
+    sc_op_spark = ShortCircuitOperator(task_id=tid_spark, dag=dag,
+                                       python_callable=spark_submit,
+                                       op_args=op_args)
+
+    tid_distcp = tid_prefix_distcp + db_name
+    py_op_disctp = PythonOperator(task_id=tid_distcp, dag=dag,
+                                  python_callable=distcp,
+                                  op_args=op_args)
+
+    sc_op_check >> sc_op_spark >> py_op_disctp
+    return dag
+
+if __name__ == '__main__':
+    for db_name in db_names:
+        create_dag(did_importer_prefix, db_name)
