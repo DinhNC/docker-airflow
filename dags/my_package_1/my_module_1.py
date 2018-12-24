ff1659382c2b43d1c57d898243ffea29aa6594d0
y2k-shubham <y2k.shubhamgupta@gmail.com>
2018-12-24 16:16:24 +0530

    import-test-2

--- dags/my_package_1/my_module_1.py
+++ dags/my_package_1/my_module_1.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+from my_package_1.my_sub_package_1 import my_module_2
+
 def print_path() -> None:
 	#print(f"PYTHONPATH = {os.environ['PYTHONPATH'].split(os.pathsep)}")
 	print("sys.path:-\n{}", *(sys.path), sep="\n")
